aba57005684b68e5f73fe54e313918b9e4d854ea
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2014-09-06 13:27:38 -0500

    lili

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,7 @@ module.exports = function(grunt) {
 		exec: {
       test: "npm test",
       jshint: "jshint .",
+      lili: "bundle exec lili .",
       aspelllint: "bundle exec aspelllint"
 		}
   });
@@ -11,6 +12,13 @@ module.exports = function(grunt) {
 
   grunt.registerTask("default", ["exec:test"]);
   grunt.registerTask("test", ["exec:test"]);
-  grunt.registerTask("lint", ["exec:jshint"]);
+
+  grunt.registerTask("lint", [
+    "exec:jshint",
+    "exec:lili"
+  ]);
+
+  grunt.registerTask("jshint", ["exec:jshint"]);
+  grunt.registerTask("lili", ["exec:lili"]);
   grunt.registerTask("aspelllint", ["exec:aspelllint"]);
 };
